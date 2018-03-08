494546bf8d02b35c9df862cab6db281832ae7684
Daniel Frey <daniel._.frey@hotmail.com>
2018-03-08 19:42:42 -0300

    Teste comentário

--- Placar Eletronico/src/com/acme/view/JFTelaPrincipal.java
+++ Placar Eletronico/src/com/acme/view/JFTelaPrincipal.java
@@ -18,6 +18,7 @@ public class JFTelaPrincipal extends javax.swing.JFrame {
         initComponents();
     }
     // Teste
+    //Teste daniel
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always

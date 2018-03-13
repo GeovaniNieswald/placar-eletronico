57df455697fb042e51afa70cdaa57d8283478734
Geovani Alex Nieswald <geovaninieswald@gmail.com>
2018-03-13 15:30:39 -0300

    Criação da estrutura e adição das classes referentes a comunicação socket

--- Controlador Placar Eletronico FX/src/com/acme/controller/FXMLTelaPrincipalController.java
+++ Controlador Placar Eletronico FX/src/com/acme/controller/FXMLTelaPrincipalController.java
@@ -1,3 +1,5 @@
+package com.acme.controller;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
---
+++ Controlador Placar Eletronico FX/src/com/acme/view/TelaPrincipal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acme.view;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Geovani Nieswald
+ */
+public class TelaPrincipal extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/acme/model/FXMLTelaPrincipal.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
